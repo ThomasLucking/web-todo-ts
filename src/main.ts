3242ad2a761b190b113e1beedc7189a20f5af2fc
thomaslucking <telucking@gmail.com>
2025-10-03 10:56:51 +0200

    feat(todo): implemented user can add tasks
    - added HTML structure (input, button, list)
    - styled interface with CSS (centered)
    - implemented Typescript logic:
      - add task on button click or Enter Key
      - display error message if input is empty
      - clear input after adding task
    
    closes: #5

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,37 @@
 import './style.css'
 
-console.log('Hello from typescript')
+const addTaskButton =
+  document.querySelector<HTMLButtonElement>('#add-todo-button')
+const inputValue = document.querySelector<HTMLInputElement>('#todo-input')
+const taskCreatedSection =
+  document.querySelector<HTMLUListElement>('#todo-elements')
+const errorMessage = document.querySelector<HTMLDivElement>('#error-message')
+
+if (!addTaskButton || !inputValue || !taskCreatedSection || !errorMessage) {
+  console.error('Missing a Dom element')
+} else {
+  const createTask = () => {
+    const value = inputValue.value.trim()
+
+    if (!value) {
+      errorMessage.textContent = 'Please enter a task'
+      return
+    }
+
+    errorMessage.textContent = ''
+    const task = document.createElement('li')
+    task.classList.add('todo-item')
+    task.textContent = value
+
+    taskCreatedSection.append(task)
+    inputValue.value = ''
+  }
+
+  addTaskButton.addEventListener('click', createTask)
+
+  inputValue.addEventListener('keydown', (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      createTask()
+    }
+  })
+}
